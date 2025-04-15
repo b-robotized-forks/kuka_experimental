086988a8f2d40e6c2122585082758cd99b9eaed9
mathias31415 <mathias31415@gmx.de>
2025-04-15 11:32:23 +0200

    setup hardware package with https://rtw.b-robotized.com/master/use-cases/ros2_control/setup_robot_hardware_interface.html

--- kuka_eki_motion_primitives_hw_interface/README.md
+++ kuka_eki_motion_primitives_hw_interface/README.md
@@ -4,3 +4,6 @@ kuka_eki_motion_primitives_hw_interface
 Driver package to control kuka robot using motion primitives like PTP, LIN and CIRC
 
 ![Licence](https://img.shields.io/badge/License-BSD-3-Clause-blue.svg)
+
+Pluginlib-Library: kuka_eki_motion_primitives_hw_interface
+Plugin: kuka_eki_motion_primitives_hw_interface/MotionPrimitivesKukaDriver (hardware_interface::SystemInterface)
---
+++ kuka_eki_motion_primitives_hw_interface/include/kuka_eki_motion_primitives_hw_interface/motion_primitives_kuka_driver.hpp
@@ -0,0 +1,73 @@
+// Copyright (c) 2025, b»robotized
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Authors: Mathias Fuhrer
+
+#ifndef KUKA_EKI_MOTION_PRIMITIVES_HW_INTERFACE__MOTION_PRIMITIVES_KUKA_DRIVER_HPP_
+#define KUKA_EKI_MOTION_PRIMITIVES_HW_INTERFACE__MOTION_PRIMITIVES_KUKA_DRIVER_HPP_
+
+#include <string>
+#include <vector>
+
+#include "kuka_eki_motion_primitives_hw_interface/visibility_control.h"
+#include "hardware_interface/system_interface.hpp"
+#include "hardware_interface/handle.hpp"
+#include "hardware_interface/hardware_info.hpp"
+#include "hardware_interface/types/hardware_interface_return_values.hpp"
+#include "rclcpp/macros.hpp"
+#include "rclcpp_lifecycle/state.hpp"
+
+namespace kuka_eki_motion_primitives_hw_interface
+{
+class MotionPrimitivesKukaDriver : public hardware_interface::SystemInterface
+{
+public:
+  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
+  hardware_interface::CallbackReturn on_init(
+    const hardware_interface::HardwareInfo & info) override;
+
+  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
+  hardware_interface::CallbackReturn on_configure(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
+  std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
+
+  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
+  std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
+
+  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
+  hardware_interface::CallbackReturn on_activate(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
+  hardware_interface::CallbackReturn on_deactivate(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
+  hardware_interface::return_type read(
+    const rclcpp::Time & time, const rclcpp::Duration & period) override;
+
+  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
+  hardware_interface::return_type write(
+    const rclcpp::Time & time, const rclcpp::Duration & period) override;
+
+private:
+  std::vector<double> hw_commands_;
+  std::vector<double> hw_states_;
+};
+
+}  // namespace kuka_eki_motion_primitives_hw_interface
+
+#endif  // KUKA_EKI_MOTION_PRIMITIVES_HW_INTERFACE__MOTION_PRIMITIVES_KUKA_DRIVER_HPP_
