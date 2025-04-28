a069c742435b3278e07e06bbde85c2267edce928
mathias31415 <mathias31415@gmx.de>
2025-04-28 16:34:56 +0200

    removed visibility control

--- kuka_eki_motion_primitives_hw_interface/include/kuka_eki_motion_primitives_hw_interface/motion_primitives_kuka_driver.hpp
+++ kuka_eki_motion_primitives_hw_interface/include/kuka_eki_motion_primitives_hw_interface/motion_primitives_kuka_driver.hpp
@@ -20,7 +20,6 @@
 #include <string>
 #include <vector>
 
-#include "kuka_eki_motion_primitives_hw_interface/visibility_control.h"
 #include "hardware_interface/system_interface.hpp"
 #include "hardware_interface/handle.hpp"
 #include "hardware_interface/hardware_info.hpp"
@@ -39,33 +38,25 @@ namespace kuka_eki_motion_primitives_hw_interface
 class MotionPrimitivesKukaDriver : public hardware_interface::SystemInterface
 {
 public:
-  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
   hardware_interface::CallbackReturn on_init(
     const hardware_interface::HardwareInfo & info) override;
 
-  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
   hardware_interface::CallbackReturn on_configure(
     const rclcpp_lifecycle::State & previous_state) override;
 
-  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
   std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
 
-  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
   std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
 
-  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
   hardware_interface::CallbackReturn on_activate(
     const rclcpp_lifecycle::State & previous_state) override;
 
-  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
   hardware_interface::CallbackReturn on_deactivate(
     const rclcpp_lifecycle::State & previous_state) override;
 
-  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
   hardware_interface::return_type read(
     const rclcpp::Time & time, const rclcpp::Duration & period) override;
 
-  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
   hardware_interface::return_type write(
     const rclcpp::Time & time, const rclcpp::Duration & period) override;
 
