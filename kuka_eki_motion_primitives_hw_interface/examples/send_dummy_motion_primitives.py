3086162479e1cd962269078786d5c79753115a5b
mathias31415 <mathias31415@gmx.de>
2025-05-15 13:00:21 +0200

    Added velocity and acceleration prams to send_dummy_motion_primitives.py

--- kuka_eki_motion_primitives_hw_interface/examples/send_dummy_motion_primitives.py
+++ kuka_eki_motion_primitives_hw_interface/examples/send_dummy_motion_primitives.py
@@ -37,56 +37,69 @@ msg_PTP_1 = MotionPrimitive()
 msg_PTP_1.type = MotionPrimitive.LINEAR_JOINT
 msg_PTP_1.joint_positions = [0.0, -1.57, 1.57, 0.0, 1.57, 0.0]
 msg_PTP_1.additional_arguments = [
-    MotionArgument(argument_name="velocity", argument_value=1.0),
-    MotionArgument(argument_name="acceleration", argument_value=1.0),
-]
+    MotionArgument(argument_name="velocity", argument_value=0.5),
+    MotionArgument(argument_name="acceleration", argument_value=0.5)]
+
 msg_PTP_1_slow = MotionPrimitive()
 msg_PTP_1_slow.type = MotionPrimitive.LINEAR_JOINT
 msg_PTP_1_slow.joint_positions = [0.0, -1.57, 1.57, 0.0, 1.57, 0.0]
 msg_PTP_1_slow.additional_arguments = [
     MotionArgument(argument_name="velocity", argument_value=0.1),
-    MotionArgument(argument_name="acceleration", argument_value=0.1),
-]
+    MotionArgument(argument_name="acceleration", argument_value=0.1)]
 
 msg_PTP_11 = MotionPrimitive()
 msg_PTP_11.type = MotionPrimitive.LINEAR_JOINT
 msg_PTP_11.joint_positions = [0.15, -1.57, 1.57, 0.0, 1.57, 0.0]
+msg_PTP_11.additional_arguments = [
+    MotionArgument(argument_name="velocity", argument_value=0.5),
+    MotionArgument(argument_name="acceleration", argument_value=0.5)]
 
 msg_PTP_12 = MotionPrimitive()
 msg_PTP_12.type = MotionPrimitive.LINEAR_JOINT
 msg_PTP_12.joint_positions = [0.3, -1.57, 1.57, 0.0, 1.57, 0.0]
+msg_PTP_12.additional_arguments = [
+    MotionArgument(argument_name="velocity", argument_value=0.5),
+    MotionArgument(argument_name="acceleration", argument_value=0.5)]
 
 msg_PTP_2 = MotionPrimitive()
 msg_PTP_2.type = MotionPrimitive.LINEAR_JOINT
 msg_PTP_2.joint_positions = [0.5, -1.57, 1.57, 0.0, 1.57, 0.0]
 msg_PTP_2.additional_arguments = [
-    MotionArgument(argument_name="velocity", argument_value=1.0),
-    MotionArgument(argument_name="acceleration", argument_value=1.0),
-]
+    MotionArgument(argument_name="velocity", argument_value=0.5),
+    MotionArgument(argument_name="acceleration", argument_value=0.5)]
+
 msg_PTP_2_slow = MotionPrimitive()
 msg_PTP_2_slow.type = MotionPrimitive.LINEAR_JOINT
 msg_PTP_2_slow.joint_positions = [0.5, -1.57, 1.57, 0.0, 1.57, 0.0]
 msg_PTP_2_slow.additional_arguments = [
     MotionArgument(argument_name="velocity", argument_value=0.1),
-    MotionArgument(argument_name="acceleration", argument_value=0.1),
-]
+    MotionArgument(argument_name="acceleration", argument_value=0.1)]
 
 msg_PTP_21 = MotionPrimitive()
 msg_PTP_21.type = MotionPrimitive.LINEAR_JOINT
 msg_PTP_21.joint_positions = [0.65, -1.57, 1.57, 0.0, 1.57, 0.0]
+msg_PTP_21.additional_arguments = [
+    MotionArgument(argument_name="velocity", argument_value=0.5),
+    MotionArgument(argument_name="acceleration", argument_value=0.5)]
 
 msg_PTP_22 = MotionPrimitive()
 msg_PTP_22.type = MotionPrimitive.LINEAR_JOINT
 msg_PTP_22.joint_positions = [0.8, -1.57, 1.57, 0.0, 1.57, 0.0]
+msg_PTP_22.additional_arguments = [
+    MotionArgument(argument_name="velocity", argument_value=0.5),
+    MotionArgument(argument_name="acceleration", argument_value=0.5)]
 
 msg_PTP_3 = MotionPrimitive()
 msg_PTP_3.type = MotionPrimitive.LINEAR_JOINT
 msg_PTP_3.joint_positions = [1.0, -1.57, 1.57, 0.0, 1.57, 0.0]
+msg_PTP_3.additional_arguments = [
+    MotionArgument(argument_name="velocity", argument_value=0.5),
+    MotionArgument(argument_name="acceleration", argument_value=0.5)]
 
 msg_LIN_1 = MotionPrimitive()
 msg_LIN_1.type = MotionPrimitive.LINEAR_CARTESIAN
 pose_L1 = PoseStamped()
-pose_L1.pose.position.x = 0.280
+pose_L1.pose.position.x = 0.240
 pose_L1.pose.position.y = 0.0
 pose_L1.pose.position.z = 0.550
 pose_L1.pose.orientation.x = 0.0
@@ -96,22 +109,20 @@ pose_L1.pose.orientation.w = 0.0
 msg_LIN_1.poses = [pose_L1]
 msg_LIN_1.additional_arguments = [
     MotionArgument(argument_name="velocity", argument_value=2.0),
-    MotionArgument(argument_name="acceleration", argument_value=2.0),
-]
+    MotionArgument(argument_name="acceleration", argument_value=2.0)]
 
 msg_LIN_1_slow = MotionPrimitive()
 msg_LIN_1_slow.type = MotionPrimitive.LINEAR_CARTESIAN
 msg_LIN_1_slow.poses = [pose_L1]
 msg_LIN_1_slow.additional_arguments = [
     MotionArgument(argument_name="velocity", argument_value=0.1),
-    MotionArgument(argument_name="acceleration", argument_value=0.1),
-]
+    MotionArgument(argument_name="acceleration", argument_value=0.1)]
 
 msg_LIN_2 = MotionPrimitive()
 msg_LIN_2.type = MotionPrimitive.LINEAR_CARTESIAN
 pose_L2 = PoseStamped()
-pose_L2.pose.position.x = 0.240
-pose_L2.pose.position.y = -0.140
+pose_L2.pose.position.x = 0.180
+pose_L2.pose.position.y = 0.140
 pose_L2.pose.position.z = 0.550
 pose_L2.pose.orientation.x = 0.0
 pose_L2.pose.orientation.y = 1.0 
@@ -120,16 +131,59 @@ pose_L2.pose.orientation.w = 0.0
 msg_LIN_2.poses = [pose_L2]
 msg_LIN_2.additional_arguments = [
     MotionArgument(argument_name="velocity", argument_value=2.0),
-    MotionArgument(argument_name="acceleration", argument_value=2.0),
-]
+    MotionArgument(argument_name="acceleration", argument_value=2.0)]
 
 msg_LIN_2_slow = MotionPrimitive()
 msg_LIN_2_slow.type = MotionPrimitive.LINEAR_CARTESIAN
 msg_LIN_2_slow.poses = [pose_L2]
 msg_LIN_2_slow.additional_arguments = [
     MotionArgument(argument_name="velocity", argument_value=0.1),
-    MotionArgument(argument_name="acceleration", argument_value=0.1),
-]
+    MotionArgument(argument_name="acceleration", argument_value=0.1)]
+
+msg_LIN_3 = MotionPrimitive()
+msg_LIN_3.type = MotionPrimitive.LINEAR_CARTESIAN
+pose_L3 = PoseStamped()
+pose_L3.pose.position.x = 0.240
+pose_L3.pose.position.y = 0.140
+pose_L3.pose.position.z = 0.550
+pose_L3.pose.orientation.x = 0.0
+pose_L3.pose.orientation.y = 1.0 
+pose_L3.pose.orientation.z = 0.0 
+pose_L3.pose.orientation.w = 0.0 
+msg_LIN_3.poses = [pose_L3]
+msg_LIN_3.additional_arguments = [
+    MotionArgument(argument_name="velocity", argument_value=2.0),
+    MotionArgument(argument_name="acceleration", argument_value=2.0)]
+
+msg_LIN_4 = MotionPrimitive()
+msg_LIN_4.type = MotionPrimitive.LINEAR_CARTESIAN
+pose_L4 = PoseStamped()
+pose_L4.pose.position.x = 0.240
+pose_L4.pose.position.y = -0.140
+pose_L4.pose.position.z = 0.550
+pose_L4.pose.orientation.x = 0.0
+pose_L4.pose.orientation.y = 1.0 
+pose_L4.pose.orientation.z = 0.0 
+pose_L4.pose.orientation.w = 0.0 
+msg_LIN_4.poses = [pose_L4]
+msg_LIN_4.additional_arguments = [
+    MotionArgument(argument_name="velocity", argument_value=2.0),
+    MotionArgument(argument_name="acceleration", argument_value=2.0)]
+
+msg_LIN_5 = MotionPrimitive()
+msg_LIN_5.type = MotionPrimitive.LINEAR_CARTESIAN
+pose_L5 = PoseStamped()
+pose_L5.pose.position.x = 0.180
+pose_L5.pose.position.y = -0.140
+pose_L5.pose.position.z = 0.550
+pose_L5.pose.orientation.x = 0.0
+pose_L5.pose.orientation.y = 1.0 
+pose_L5.pose.orientation.z = 0.0 
+pose_L5.pose.orientation.w = 0.0 
+msg_LIN_5.poses = [pose_L5]
+msg_LIN_5.additional_arguments = [
+    MotionArgument(argument_name="velocity", argument_value=2.0),
+    MotionArgument(argument_name="acceleration", argument_value=2.0)]
 
 msg_moveC_1 = MotionPrimitive()
 msg_moveC_1.type = MotionPrimitive.CIRCULAR_CARTESIAN
@@ -152,16 +206,14 @@ pose_C1_goal.pose.orientation.w = 0.05
 msg_moveC_1.poses = [pose_C1_goal, pose_C1_via] 
 msg_moveC_1.additional_arguments = [
     MotionArgument(argument_name="velocity", argument_value=2.0),
-    MotionArgument(argument_name="acceleration", argument_value=2.0),
-]
+    MotionArgument(argument_name="acceleration", argument_value=2.0)]
 
 msg_moveC_1_slow = MotionPrimitive()
 msg_moveC_1_slow.type = MotionPrimitive.CIRCULAR_CARTESIAN
 msg_moveC_1_slow.poses = [pose_C1_goal, pose_C1_via] 
 msg_moveC_1_slow.additional_arguments = [
     MotionArgument(argument_name="velocity", argument_value=0.1),
-    MotionArgument(argument_name="acceleration", argument_value=0.1),
-]
+    MotionArgument(argument_name="acceleration", argument_value=0.1)]
 
 
 
@@ -180,7 +232,9 @@ class MotionPublisher(Node):
         # self.messages = [msg_PTP_1, msg_PTP_11, msg_PTP_12, msg_PTP_2, msg_PTP_21, msg_PTP_21, msg_PTP_22, msg_PTP_3, msg_start_sequence, msg_PTP_1, msg_PTP_11, msg_PTP_12, msg_PTP_2, msg_PTP_21, msg_PTP_21, msg_PTP_22, msg_PTP_3, msg_end_sequence]
         # self.messages = [msg_PTP_1, msg_PTP_2, msg_PTP_1_slow, msg_PTP_2_slow]
         # self.messages = [msg_PTP_1, msg_LIN_1, msg_LIN_2, msg_LIN_1_slow, msg_LIN_2_slow]
-        self.messages = [msg_PTP_2, msg_moveC_1, msg_PTP_2_slow, msg_moveC_1_slow]
+        # self.messages = [msg_PTP_2, msg_moveC_1, msg_PTP_2_slow, msg_moveC_1_slow]
+        # self.messages = [msg_LIN_2, msg_LIN_3, msg_LIN_1, msg_LIN_4, msg_LIN_5, msg_start_sequence, msg_LIN_2, msg_LIN_3, msg_LIN_1, msg_LIN_4, msg_LIN_5, msg_end_sequence]
+        self.messages = [msg_LIN_2, msg_LIN_3, msg_LIN_4, msg_LIN_5, msg_start_sequence, msg_LIN_2, msg_LIN_3, msg_LIN_4, msg_LIN_5, msg_end_sequence]
         self.get_logger().info(f"Number of messages: {len(self.messages)}")
 
         # Initial delay before the first message
