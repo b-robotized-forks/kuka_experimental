a1b639a63211c4e5e9b3aa355063f1f71cdab6b2
mathias31415 <mathias31415@gmx.de>
2025-05-19 15:29:37 +0200

    added rsi example usage with python script for r2e cell

---
+++ kuka_rsi_hw_interface/kuka_rsi_hw_interface/send_joint_positions.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+import time
+
+class TrajectoryPublisher(Node):
+    def __init__(self):
+        super().__init__('trajectory_publisher')
+        self.publisher_ = self.create_publisher(JointTrajectory, '/position_trajectory_controller/joint_trajectory', 10)
+        time.sleep(1)  # Allow time for publisher to be ready
+
+    def publish_trajectory_startponit(self):
+        msg = JointTrajectory()
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.joint_names = ["joint_a1", "joint_a2", "joint_a3", "joint_a4", "joint_a5", "joint_a6"]
+        msg.points = [JointTrajectoryPoint(positions=[0, -1.57, 1.57, 0.0, 1.57, 0.0], velocities=[0.0]*6, time_from_start=rclpy.duration.Duration(seconds=3).to_msg())]
+        self.publisher_.publish(msg)
+        self.get_logger().info('Trajectory startpoint published successfully.')
+
+    def publish_trajectory_sequence(self):
+        msg = JointTrajectory()
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.joint_names = ["joint_a1", "joint_a2", "joint_a3", "joint_a4", "joint_a5", "joint_a6"]
+
+        # Define multiple trajectory points
+        points = [
+            # JointTrajectoryPoint(positions=[0, -1.57, 1.57, 0.0, 1.57, 0.0], velocities=[0.0]*6, time_from_start=rclpy.duration.Duration(seconds=).to_msg()),
+            JointTrajectoryPoint(positions=[0, -1.1, 1.0, 0.0, 1.57, 0.0], velocities=[0.0]*6, time_from_start=rclpy.duration.Duration(seconds=0.5).to_msg()),
+            JointTrajectoryPoint(positions=[0.7, -0.9, 0.7, 0.0, 1.57, 0.0], velocities=[0.0]*6, time_from_start=rclpy.duration.Duration(seconds=1.0).to_msg()),
+            JointTrajectoryPoint(positions=[1.57, -1.57, 1.57, 0.0, 1.57, 0.0], velocities=[0.0]*6, time_from_start=rclpy.duration.Duration(seconds=1.5).to_msg()),
+            JointTrajectoryPoint(positions=[0, -1.57, 1.57, 0.0, 1.57, 0.0], velocities=[0.0]*6, time_from_start=rclpy.duration.Duration(seconds=2.0).to_msg()),
+            JointTrajectoryPoint(positions=[0, -1.1, 1.0, 0.0, 1.57, 0.0], velocities=[0.0]*6, time_from_start=rclpy.duration.Duration(seconds=2.5).to_msg()),
+            JointTrajectoryPoint(positions=[-0.7, -0.9, 0.7, 0.0, 1.57, 0.0], velocities=[0.0]*6, time_from_start=rclpy.duration.Duration(seconds=3.0).to_msg()),
+            JointTrajectoryPoint(positions=[-1.57, -1.57, 1.57, 0.0, 1.57, 0.0], velocities=[0.0]*6, time_from_start=rclpy.duration.Duration(seconds=3.5).to_msg()),
+            JointTrajectoryPoint(positions=[0, -1.57, 1.57, 0.0, 1.57, 0.0], velocities=[0.0]*6, time_from_start=rclpy.duration.Duration(seconds=4.0).to_msg()),
+        ]
+
+        msg.points = points
+        self.publisher_.publish(msg)
+        self.get_logger().info('Trajectory sequence published successfully.')
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = TrajectoryPublisher()
+    node.publish_trajectory_startponit()
+    time.sleep(4)
+    for _ in range(5):
+        node.publish_trajectory_sequence()
+        time.sleep(4)   # Adjust time to sequence length
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
