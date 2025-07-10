8f0e5fba5fe021251879865f85a3ad553825d856
mathias31415 <mathias31415@gmx.de>
2025-07-10 09:20:45 +0200

    changed tf2 include to tf2_geometry_msgs

--- kuka_eki_motion_primitives_hw_interface/src/motion_primitives_kuka_driver.cpp
+++ kuka_eki_motion_primitives_hw_interface/src/motion_primitives_kuka_driver.cpp
@@ -16,8 +16,7 @@
 
 #include <limits>
 #include <vector>
-#include <tf2/LinearMath/Quaternion.h>
-#include <tf2/LinearMath/Matrix3x3.h>
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
 
 #include "kuka_eki_motion_primitives_hw_interface/motion_primitives_kuka_driver.hpp"
 #include "hardware_interface/types/hardware_interface_type_values.hpp"
